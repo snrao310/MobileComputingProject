95796452a08a7408fac284529efcdfb97210f8f9
snrao310 <sameerana.nrao@yahoo.com>
2016-04-12 18:14:35 -0700

    Fragments for maps done. Maps need to be implemeted inside map fragment

--- app/src/main/java/com/mobilecomputing/group3/mcproject/SignupActivity.java
+++ app/src/main/java/com/mobilecomputing/group3/mcproject/SignupActivity.java
@@ -1,7 +1,12 @@
 package com.mobilecomputing.group3.mcproject;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        FragmentManager fragmentManager= getFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        SignupFragment signupFragment=new SignupFragment();
+        fragmentTransaction.add(R.id.frameCont, signupFragment);
+        fragmentTransaction.commit();
+
+        Button setLoc=(Button) findViewById(R.id.locSelect);
+        setLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_signup);
+                FragmentManager fragmentManager= getFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                MapFragment mapFragment=new MapFragment();
+                fragmentTransaction.replace(R.id.frameCont,mapFragment);
+                fragmentTransaction.commit();
+            }
+        });
+    }
+
+    public void onSetLoc(View view)
+    {
+        //Intent intent = new Intent(this, MapsActivity.class);
+        //startActivity(intent);
     }
 }
