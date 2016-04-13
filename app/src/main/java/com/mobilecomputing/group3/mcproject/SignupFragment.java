9c9d4f2649d35547b42d860fca8288031253de7b
snrao310 <sameerana.nrao@yahoo.com>
2016-04-12 18:22:30 -0700

    Fragments for maps done. Maps need to be implemeted inside map fragment

---
+++ app/src/main/java/com/mobilecomputing/group3/mcproject/SignupFragment.java
@@ -0,0 +1,20 @@
+package com.mobilecomputing.group3.mcproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by snrao on 12/4/16.
+ */
+public class SignupFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.signup_fragment,container,false);
+    }
+}
