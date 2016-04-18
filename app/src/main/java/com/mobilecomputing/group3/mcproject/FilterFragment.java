43ed9fe7f479d691d7b4af7217ddc783a5e538c2
snrao310 <sameerana.nrao@yahoo.com>
2016-04-18 14:52:47 -0700

    Included aoi and skillset

---
+++ app/src/main/java/com/mobilecomputing/group3/mcproject/FilterFragment.java
@@ -0,0 +1,7 @@
+package com.mobilecomputing.group3.mcproject;
+
+/**
+ * Created by snrao on 4/18/16.
+ */
+public class FilterFragment {
+}
---
+++ app/src/main/java/com/mobilecomputing/group3/mcproject/RequestsFragment.java
@@ -0,0 +1,8 @@
+package com.mobilecomputing.group3.mcproject;
+
+/**
+ * Created by snrao on 4/18/16.
+ */
+public class RequestsFragment {
+
+}
